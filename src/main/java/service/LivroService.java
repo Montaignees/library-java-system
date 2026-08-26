d8a1e172c7d90808c47f9244bd7225937d9c8e30
Lucas de Montaigne Teixeira Brito <montaignelucas01@gmail.com>
2026-08-25 22:24:16 -0300

    feat: finalize LivroService class

--- src/main/java/service/LivroService.java
+++ src/main/java/service/LivroService.java
@@ -1,9 +1,15 @@
 package service;
+
 import model.Livro;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LivroService {
+    enum ResultadoOperacao {
+        SUCESSO,
+        LIVRO_NAO_ENCONTRADO,
+        QUANTIDADE_INVALIDA
+    }
 
     private List<Livro> livros = new ArrayList<>();
 
@@ -12,14 +18,30 @@ public class LivroService {
     }
 
     public Livro buscarLivro(String isbn) {
-        for (int i = 0; i < livros.size(); i++) {
-
-            Livro livroAtual = livros.get(i);
-
-            if (isbn.equals(livroAtual.getIsbn())) {
-                return livroAtual;
+        for (Livro livro : livros) {
+            if (livro.getIsbn().equals(isbn)) {
+                return livro;
             }
         }
         return null;
+    }
+
+    public List<Livro> listarLivros() {
+        return livros;
+    }
+
+    public ResultadoOperacao addExemplares(String isbn, int exemplares) {
+
+        Livro livroAtual = buscarLivro(isbn);
+
+        if (exemplares <= 0) {
+            return ResultadoOperacao.QUANTIDADE_INVALIDA;
+        }
+        if (livroAtual == null) {
+            return ResultadoOperacao.LIVRO_NAO_ENCONTRADO;
+        }
+
+        livroAtual.setExemplares(livroAtual.getExemplares() + exemplares);
+        return ResultadoOperacao.SUCESSO;
     }
 }
